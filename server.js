6687ce64664aa2b650587eed9da32220a618df6c
Luis03 <rage.875@gmail.com>
2019-06-06 19:51:55 -0500

    clase_14 updt server.js express-routing

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const express = require('express')
 const app = express()
 const port = 9000
 
+// Crea los handlers para la siguiente ruta ‘/’ 
+// con los siguientes métodos: get, post, put y delete.
+// Cada handler debe responder con el siguiente mensaje 
+// Contestando una petición por <método>'
+
 app.get('/', function (req, res) {
-res.send('Hello World!')
+    res.send('Contestando una petición por <get>')
+})
+
+app.post('/', function (req, res) {
+    res.send('Contestando una petición por <post>')
+})
+
+app.put('/', function (req, res) {
+    res.send('Contestando una petición por <delete>')
+})
+
+app.delete('/', function (req, res) {
+    res.send('Contestando una petición por <delete>')
 })
 
 app.listen(port, function () { 
